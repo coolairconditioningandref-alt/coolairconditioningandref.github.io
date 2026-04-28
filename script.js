38bbcfce07ad90beaba031c4b27272374e119470
coolairconditioningandref-alt <coolairconditioningandref@gmail.com>
2026-04-27 19:39:28 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
-function scrollToContact() {
-  document.getElementById("contact").scrollIntoView({
-    behavior: "smooth"
+let cart = [];
+
+function addToCart(service) {
+  cart.push(service);
+  displayCart();
+}
+
+function displayCart() {
+  let list = document.getElementById("cartItems");
+  list.innerHTML = "";
+
+  cart.forEach(item => {
+    let li = document.createElement("li");
+    li.innerText = item;
+    list.appendChild(li);
   });
+}
+
+function checkout() {
+  if (cart.length === 0) {
+    alert("Cart is empty!");
+  } else {
+    alert("Booking Confirmed!");
+    cart = [];
+    displayCart();
+  }
+}
+
+function scrollToServices() {
+  document.getElementById("services").scrollIntoView();
 }
